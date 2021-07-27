23b12ebfdb93a09ae22a5c69878ba1df9aa048f5
Tiago Weber <webertiagof@gmail.com>
2021-07-27 17:46:52 -0300

    Add connection to database

--- index.php
+++ index.php
@@ -2,8 +2,17 @@
 
 require 'functions.php';
 
-$animals = ['dog', 'cat'];
+try {
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', '123456');
+    $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+} catch (\PDOException $e) {
+    echo $e->getMessage();
+}
 
-dd($animals);
+$statement = $pdo->prepare('select * from todos');
+
+$statement->execute();
+
+$tasks = $statement->fetchAll(PDO::FETCH_OBJ);
 
 require 'index.view.php';
--- index.view.php
+++ index.view.php
@@ -5,19 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
 </head>
 <body>
     <header>
-        <h1>
-            <?= $greeting; ?>
-        </h1>
+        <ul>
+            <?php foreach ($tasks as $task) : ?>
+                <li>
+                    <?php if ($task->completed) : ?>
+                        <strike><?= $task->description; ?></strike>
+                    <?php else: ?>
+                        <?= $task->description; ?>
+                    <?php endif; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
     </header>
 </body>
 </html>
