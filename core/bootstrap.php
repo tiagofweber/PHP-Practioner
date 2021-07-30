4e4f480e70b883522efdea4eaf069b8b5cf098f2
Tiago Weber <webertiagof@gmail.com>
2021-07-30 20:47:00 -0300

    Refactor the bootstrap file

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,10 +1,18 @@
 <?php
 
-$config = require 'config.php';
+$app = [];
+
+$app['config'] = require 'config.php';
 
 require 'core/Router.php';
 require 'core/Request.php';
 require 'core/database/Connection.php';
 require 'core/database/QueryBuilder.php';
 
-return new QueryBuilder(Connection::make($config['database']));
+$app['database'] = new QueryBuilder(Connection::make($app['config']['database']));
+
+function dd ($data) {
+    echo '<pre>';
+    die(var_dump($data));
+    echo '</pre>';
+}
