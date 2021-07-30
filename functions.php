008a4aeaf7e846d166d94877cd17fe910c4e11e2
Tiago Weber <webertiagof@gmail.com>
2021-07-30 16:07:43 -0300

    Separe the connection to database into a function in the functions file

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
 <?php
 
+function connectToDb() {
+    try {
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', '123456');
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (\PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
 function dd ($data) {
     echo '<pre>';
     die(var_dump($data));
--- index.php
+++ index.php
@@ -3,12 +3,7 @@
 require 'functions.php';
 require 'Task.php';
 
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', '123456');
-    $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-} catch (\PDOException $e) {
-    echo $e->getMessage();
-}
+$pdo = connectToDb();
 
 $statement = $pdo->prepare('select * from todos');
 
