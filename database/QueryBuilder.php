21c9f28fb332f4b72a580009272102d64813b781
Tiago Weber <webertiagof@gmail.com>
2021-07-30 18:50:49 -0300

    Delete Task class

--- database/QueryBuilder.php
+++ database/QueryBuilder.php
@@ -9,12 +9,12 @@ class QueryBuilder
         $this->pdo = $pdo;
     }
 
-    public function selectAll($table)
+    public function selectAll($table, $intoClass)
     {
         $statement = $this->pdo->prepare("select * from {$table}");
 
         $statement->execute();
 
-        return $statement->fetchAll(PDO::FETCH_CLASS);
+        return $statement->fetchAll(PDO::FETCH_CLASS, $intoClass);
     }
 }
