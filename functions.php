ccec05cb7f4f38250cfb22fb43c10765b720c9de
Tiago Weber <webertiagof@gmail.com>
2021-07-30 18:27:39 -0300

    Create class QueryBuilder

--- functions.php
+++ functions.php
@@ -1,13 +1,5 @@
 <?php
 
-function fetchAllTasks($pdo) {
-    $statement = $pdo->prepare('select * from todos');
-
-    $statement->execute();
-
-    return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
-}
-
 function dd ($data) {
     echo '<pre>';
     die(var_dump($data));
--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 
 require 'functions.php';
 require 'database/Connection.php';
+require 'database/QueryBuilder.php';
 require 'Task.php';
 
 $pdo = Connection::make();
 
-$tasks = fetchAllTasks($pdo);
+$query = new QueryBuilder($pdo);
+
+$tasks = $query->selectAll('todos');
 
 require 'index.view.php';
