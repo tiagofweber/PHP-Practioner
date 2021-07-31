6d92362d39b3a365f4662c04c98b3470d06df303
Tiago Weber <webertiagof@gmail.com>
2021-07-31 13:11:26 -0300

    Refactoring views

--- views/index.view.php
+++ views/index.view.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <nav>
-        <ul>
-            <li><a href="/about.php">About Page</a></li>
-            <li><a href="/contact.php">Contact Page</a></li>
-        </ul>
-    </nav>
+<?php require('partials/head.php'); ?>
 
     <h1>My Tasks</h1>
     
@@ -27,5 +13,5 @@
             </li>
         <?php endforeach; ?>
     </ul>
-</body>
-</html>
+
+<?php require('partials/footer.php'); ?>
