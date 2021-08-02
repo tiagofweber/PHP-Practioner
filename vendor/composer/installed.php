f91b577c7ec1c340c137667df4e61487ea28278b
Tiago Weber <webertiagof@gmail.com>
2021-08-02 15:40:06 -0300

    Istall composer autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '71690adefec75690994be13f018fa9cce25f006c',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '71690adefec75690994be13f018fa9cce25f006c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
